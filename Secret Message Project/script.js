2d8ce879d8e6389629b3dcdb82e241edfac27794
Abdi Torres Romero <Atorre278@west-mec.local>
2021-10-27 18:26:33 -0700

    push

--- Secret Message Project/script.js
+++ Secret Message Project/script.js
@@ -4,9 +4,15 @@ const fs = require("fs");
 const input = fs.readFileSync("./jenkins.txt", 'utf8');
 
 //1.
-const re = /[{@}][a-zA-Z]{0,18}\S/g
+const re = /[{@}][a-zA-Z]\s{0,18}\S\w+/g
 
 
 console.log(input.match(re));
+
+//2. 
+const re2 = /[{@}][a-zA-Z]\s{0,18}\S\w+/g
+
+
+console.log(input.match(re2));
 
 
