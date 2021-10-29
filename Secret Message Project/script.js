833296dbd68555c0136477c43e2cf22da69dd2aa
Abdi Torres Romero <Atorre278@west-mec.local>
2021-10-28 17:50:31 -0700

    push

--- Secret Message Project/script.js
+++ Secret Message Project/script.js
@@ -10,7 +10,7 @@ const re = /[{@}][a-zA-Z]\s{0,18}\S\w+/g
 console.log(input.match(re));
 
 //2. 
-const re2 = /[{@}][a-zA-Z]\s{0,18}\S\w+/g
+const re2 = /.\S\b\d+(°|’|.|”)[a-zA-Z]\d\S.\b/g;
 
 
 console.log(input.match(re2));
